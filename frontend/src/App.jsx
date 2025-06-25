4e3bf4cb56b51d6d7b4d3635ab1ae2cb0ce7973c
ojasvsingh71 <ojasvsingh0@gmail.com>
2025-06-25 15:53:43 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App(){
+
+  return (
+    <div className="flex justify-center items-center bg-indigo-400 h-screen">
+      <h1 className="text-4xl font-bold">MiniMoo</h1>
+    </div>
+  );
+}
+
+export default App;
